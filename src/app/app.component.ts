f84aa8020babe6e15031cfaeee451800479fa539
Aneeshahamed <aneeshahamed.qs@gmail.com>
2022-11-13 13:21:08 +0530

    login page design completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,28 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'FullStack';
-  log_email: any;
-  log_password:any;
-
-  reg_email: any;
-  reg_password:any;
-  reg_confirm_password:any;
   
   constructor() { }
-  hide : boolean = true;
 
-  showHide() {
-    this.hide = !this.hide;
-  }
   ngOnInit() {
     
   }
-  login(){
-
-  }
-  Register() {
-    
-  }
- 
 
 }
